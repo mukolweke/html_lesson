934bd60ba08972190f6873b51734d2e76bd4f435
mukolweke <michaelolukaka@gmail.com>
2018-06-11 19:29:33 +0300

    test application

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>HTML CLASS</title>
+</head>
+<body>
+<h4>HTML /HTML5 LESSONS</h4>
+<ul>
+    <li><a href="canvas.html">Canvas</a></li>
+    <li><a href="svg.html">SVG</a> </li>
+    <li><a href="google_map.html">Google Maps</a> </li>
+</ul>
+<h4>Basics in Multimedia</h4>
+<ul>
+    <li><a href="video.html">Video</a></li>
+    <li><a href="audio.html">Audio</a></li>
+</ul>
+<h4>Basics in API's</h4>
+<ul>
+    <li><a href="geolocation.html">Geolocation</a></li>
+    <li><a href="dragdrop.html">Drag & Drop</a> </li>
+</ul>
+
+</body>
+</html>
